73fe26f515a6e8a14c101b0fbe944ba585ec39cf
beloha <belohadk97@gmail.com>
2018-03-29 20:49:39 +0300

    --feature --add split chunk optimization

--- Module2_Webpack_Advanced/src/webpack/env/config.js
+++ Module2_Webpack_Advanced/src/webpack/env/config.js
@@ -50,6 +50,27 @@ module.exports = () => {
                     exclude: /node_module/
                 }
             ]
+        }, 
+        optimization: {
+            splitChunks: {
+                chunks: "async",
+                minSize: 30000,
+                minChunks: 1,
+                maxAsyncRequests: 5,
+                maxInitialRequests: 3,
+                name: true,
+                cacheGroups: {
+                    default: {
+                        minChunks: 2,
+                        priority: -20,
+                        reuseExistingChunk: true
+                    },
+                    vendors: {
+                        test: /[\\/]node_modules[\\/]/,
+                        priority: -10
+                    }
+                }
+            }
         },
         plugins: [
             new CleanWebpackPlugin(['dist']),
