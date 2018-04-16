a7cf39ae2ec146e7204a58f6cbc3d89139249bae
beloha <belohadk97@gmail.com>
2018-04-16 23:21:13 +0300

    working on that

--- Module4_RP/index.js
+++ Module4_RP/index.js
@@ -6,37 +6,41 @@ const Observable = require('./Observable.js');
 const randomSymbol = require('./randomSymbol.js');
 const {OfRandmSymbols, RandSymbolDataSource} = randomSymbol;
 
-const subscription = (
-        new OfRandmSymbols((observer) => {
-            const randSymbolDataSource = new RandSymbolDataSource();
+const observable = (
+    new OfRandmSymbols((observer) => {
+        const randSymbolDataSource = new RandSymbolDataSource();
 
-            randSymbolDataSource.onError = (err) => observer.console.error(err);
-            randSymbolDataSource.onData = (data) => observer.next(data);
-            randSymbolDataSource.onComplete = () => observer.complete();
+        randSymbolDataSource.onError = (err) => observer.error(err);
+        randSymbolDataSource.onData = (data) => observer.next(data);
+        randSymbolDataSource.onComplete = () => observer.complete();
 
-            return () => randSymbolDataSource.destroy();
-        })
-    )
-    .subscribe(
-        v => console.log(v),
-        err => console.log('ERROR', err),
-        () => console.log('COMPLETE')
-    );
+        return () => randSymbolDataSource.destroy();
+    })
+);
 
-//setTimeout(subscription, 3000);
+console.log(observable);
+// observable.subscribe(
+//     v => console.log(v),
+//     err => console.log('ERROR', err),
+//     () => console.log('COMPLETE')
+// );
+
+                                                                                                                                    ////setTimeout(subscription, 3000);
 /*
     To here. End of 2 task
 */
 
 
 /*
-    To see 3 uncomment from here
+    To see 3 uncomment 2 task and from here
 */
-// const myFilter = require('./operators/myFilter.js');
+const myFilter = require('./operators/myFilter.js');
 
-// (new Observable())
-//     .myFilter('asdasd')
-//     .subscribe(v => console.log(v));
+observable
+    .myFilter('asdasd')
+    .subscribe(v => console.log(v),
+    err => console.log('ERROR', err),
+    () => console.log('COMPLETE'));
     /*
     To here. End of 3 task
 */
--- Module4_RP/operators/myFilter.js
+++ Module4_RP/operators/myFilter.js
@@ -1,15 +1,19 @@
 const Observable = require('./../Observable.js');
+const {OfRandmSymbols} = require('./../randomSymbol.js');
 
 // Bad aproach. Imagine, that consumer, that use library with a prototype inheritance, assume, that myFilter is in library
 // and he shouldn't import it. And you, in your library, come to conclusion, that you need to delete myFilter from your imports. And 
 // you delete in all places carefully. But, your library consumer will face with a errors, that Observable don't has such method as 'myFilter'
-Observable.prototype.myFilter = (string) => {
-    return {
-        subscribe: (observer) => {
-            console.log(observer);
-            const observable = new Observable(observer);
+OfRandmSymbols.prototype.myFilter = (string, value) => {
+    console.log('myFilter Observer', string, value);
+    const filter = (x) => { return 'sss'; }
+    return new OfRandmSymbols((observer) => {
+        const myFilterObserver = {
+            next: (x) => observer.next(filter(x)),
+            error: (err) => observer.error(err),
+            complete: () => observer.complete()
+        }
 
-            return observable.unsubscribe();
-        } 
-    }
+        return this.subscribe(myFilterObserver);
+    })
 };
