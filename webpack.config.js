1c5c1516f662cbe31cb0b5629a760c4eb4fd0596
beloha <belohadk97@gmail.com>
2018-04-22 15:57:21 +0300

    added vue env

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './Module4_RP/index.js',
+  entry: './Module5_Frameworks/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
