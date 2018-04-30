663142b44c54abc54134041868d7326878b954b0
Danylo Bilokha <danbilokha@gmail.com>
2018-04-30 13:42:16 +0300

    fixed comments

--- Module6_ReactNative/src/app/components/SomeStuff/SomeStuff.js
+++ Module6_ReactNative/src/app/components/SomeStuff/SomeStuff.js
@@ -45,18 +45,9 @@ class SomeStuff extends PureComponent {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
     title: {
       fontSize: 20,
       fontWeight: 'bold',
-    },
-    caption: {
-        fontWeight: 'bold'
     }
 });
 
